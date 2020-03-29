025a7f55f283ade0c8c69d59310a412cb09d609c
agnysbueno <agnysbueno@gmail.com>
2020-03-29 19:13:33 -0300

    Testes da aplicação

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,4 +1,4 @@
-const crypto = require('crypto');
+const generateUniqueId = require('../utils/generateUniqueId');
 const connection = require('../database/connection');
 
 module.exports = {
@@ -13,8 +13,7 @@ module.exports = {
     async create(req, res) {
         const {name, email, whatsapp, city, uf} = req.body;
         
-        //gerar uma string hexadecimal aleatória de 4 bytes 
-        const id = crypto.randomBytes(4).toString('HEX');
+        const id = generateUniqueId();
 
         //await: o programa vai esperar a inserção dos dados ser concluída para executar outra ação
         await connection('ongs').insert({
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://localhost:3333", //base da url que se repete em todas as rotas
+    baseURL: "http://localhost:3000", //base da url que se repete em todas as rotas
 })
 
 export default api;
